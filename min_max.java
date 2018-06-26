bd5232cc58fec621e1c59489e641de7b81811075
Rezgui Baha Eddinne <rezguibahaeddinne@gmail.com>
2018-06-26 20:50:53 +0100

    init
    min max logic in java

---
+++ min_max.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class MinMax {
+    public static int[] minMax(int[] arr) {
+         Arrays.sort(arr);
+        return new int[]{arr[0],arr[arr.length-1]};
+    }
+     public static int[] minMax_second(int[] arr) {
+       return new int[]{Arrays.stream(arr).min().getAsInt(), Arrays.stream(arr).max().getAsInt()};
+    }
+}
